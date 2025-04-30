1f64c8ca03c63ef5395a9b02b7807f9446220c1b
Gustavo <gustavo.nascimento196@etec.sp.gov.br>
2025-04-30 00:03:39 -0300

    FilaBanco

---
+++ AtividadeDS/FilaBanco.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeDS
+{
+    class Fila
+    {
+        private string[] fila = new string[10];
+        private int count = 0;
+
+        public void CadastrarCliente(string nome)
+        {
+            if (count < fila.Length)
+            {
+                fila[count++] = nome;
+                Console.WriteLine("Cliente {0} cadastrado com sucesso.", nome);
+            }
+            else
+            {
+                Console.WriteLine("Capacidade maxima atigida, não é possível cadastrar mais clientes.");
+            }
+        }
+
+        public void ListarFila()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Nenhum cliente na fila.");
+            }
+            else
+            {
+                Console.WriteLine("Clientes na fila:");
+                for (int i = 0; i < count; i++)
+                {
+                    Console.WriteLine($"{i + 1}. {fila[i]}");
+                }
+            }
+        }
+
+        public void IncluirClientePrioritario(string nome)
+        {
+            if (count < fila.Length)
+            {
+                for (int i = count; i > 0; i--)
+                {
+                    fila[i] = fila[i - 1];
+                }
+                fila[0] = nome;
+                count++;
+                Console.WriteLine("Cliente prioritário {0} adicionado á fila.", nome);
+            }
+            else
+            {
+                Console.WriteLine("Fila cheia .");
+            }
+        }
+
+        public void AtenderCliente()
+        {
+            if (count == 0)
+            {
+                Console.WriteLine("Nenhum cliente na fila .");
+            }
+            else
+            {
+                Console.WriteLine("Cliente {0} atendido.", fila[0]);
+                for (int i = 1; i < count; i++)
+                {
+                    fila[i - 1] = fila[i];
+                }
+                fila[--count] = null; 
+            }
+        }
+    }
+}
---
+++ AtividadeDS/Menu.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AtividadeDS
+{
+    class Menu
+    {
+        private Fila fila = new Fila();
+
+        public void ExibirMenu()
+        {
+            string opcao;
+            do
+            {
+                Console.WriteLine("\nMenu:");
+                Console.WriteLine("1 - Cadastrar cliente");
+                Console.WriteLine("2 - Listar fila de clientes");
+                Console.WriteLine("3 - Incluir cliente prioritário");
+                Console.WriteLine("4 - Atender cliente");
+                Console.WriteLine("q - Sair");
+                Console.Write("Escolha uma opção: ");
+                opcao = Console.ReadLine();
+
+                switch (opcao)
+                {
+                    case "1":
+                    
+                        Console.Write("Digite o nome do cliente: ");
+                        string nome = Console.ReadLine();
+                        fila.CadastrarCliente(nome);
+                        break;
+                    case "2":
+                        fila.ListarFila();
+                        break;
+                    case "3":
+                        Console.Write("Digite o nome do cliente prioritário: ");
+                        string prioritario = Console.ReadLine();
+                        fila.IncluirClientePrioritario(prioritario);
+                        break;
+                    case "4":
+                        fila.AtenderCliente();
+                        break;
+                    case "q":
+                        Console.WriteLine("Saindo do sistema...");
+                        break;
+                    default:
+                        Console.WriteLine("Opção inválida! Tente novamente.");
+                        break;
+                }
+            } while (opcao != "q");
+        }
+    }
+}
